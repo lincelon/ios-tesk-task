2cccd75a54f31e4cbec406ce2c8d986d9686de34
Andrii Bodnar <andrii.bodnar@gen.tech>
2025-01-10 17:24:27 +0100

    Updated the comment in ServicesAssembler

--- TransactionsTestTask/Services/ServicesAssembler.swift
+++ TransactionsTestTask/Services/ServicesAssembler.swift
@@ -7,6 +7,10 @@
 /// Services Assembler is used for Dependency Injection
 /// There is an example of a _bad_ services relationship built on `onRateUpdate` callback
 /// This kind of relationship must be refactored with a more convenient and reliable approach
+///
+/// It's ok to move the logging to model/viewModel/interactor/etc when you have 1-2 modules in your app
+/// Imagine having rate updates in 20-50 diffent modules
+/// Make this logic not depending on any module
 enum ServicesAssembler {
     
     // MARK: - BitcoinRateService
