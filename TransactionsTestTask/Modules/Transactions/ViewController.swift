cf9318357cbf5755abdf374c6a984c15e6521cff
Maksym Soroka <Maxim40a@icloud.com>
2025-03-07 14:04:11 +0200

    organize file structure

--- TransactionsTestTask/Modules/Transactions/ViewController.swift
+++ TransactionsTestTask/Modules/Transactions/ViewController.swift
@@ -6,9 +6,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 }
